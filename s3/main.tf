2263b20d4fd59936941b3d554f0d65489b6e0db5
Priyanshu Banwala <priyanshubanwala1222@gmail.com>
2026-06-18 22:08:49 +0530

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -1,46 +1,46 @@
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = var.s3-bucket-name
+# resource "aws_s3_bucket" "s3_bucket" {
+#  bucket = var.s3-bucket-name
 
-  lifecycle {
-    prevent_destroy = false # false to delete
-  }
+#  lifecycle {
+ #   prevent_destroy = false # false to delete
+ # }
 
-  tags = {
-    Name        = "s3-bucket-as-2"
-    Environment = "Dev"
-  }
-}
+  #tags = {
+   # Name        = "s3-bucket-as-2"
+    #Environment = "Dev"
+  #}
+#}
 
-resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
-  bucket = aws_s3_bucket.s3_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+#resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
+  #bucket = aws_s3_bucket.s3_bucket.id
+  #versioning_configuration {
+    #status = "Enabled"
+  #}
+#}
 
-resource "aws_s3_bucket_public_access_block" "tf_state_pab" {
-  bucket                  = aws_s3_bucket.s3_bucket.id
-  block_public_acls       = true
-  ignore_public_acls      = true
-  block_public_policy     = true
-  restrict_public_buckets = true
-}
+#resource "aws_s3_bucket_public_access_block" "tf_state_pab" {
+  #bucket                  = aws_s3_bucket.s3_bucket.id
+  #block_public_acls       = true
+  #ignore_public_acls      = true
+  #block_public_policy     = true
+  #restrict_public_buckets = true
+#}
 
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name         = "terraform-state-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+#resource "aws_dynamodb_table" "terraform_state_lock" {
+  #name         = "terraform-state-lock"
+  #billing_mode = "PAY_PER_REQUEST"
+  #hash_key     = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+  #attribute {
+   # name = "LockID"
+    #type = "S"
+  #}
 
-  tags = {
-    Name        = "terraform-state-lock"
-    Environment = "Dev"
-  }
-}
+  #tags = {
+   # Name        = "terraform-state-lock"
+    #Environment = "Dev"
+  #}
+#}
 
 # resource "aws_iam_user" "user-task-2" {
 #   name = "task-2-User"
