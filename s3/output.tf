122ca30ffc0a13741e5f5f2e8d222c1834885d69
Priyanshu Banwala <priyanshubanwala1222@gmail.com>
2026-06-18 22:09:14 +0530

    Comment out outputs in output.tf
    Comment out output blocks for S3 bucket and DynamoDB table.

--- s3/output.tf
+++ s3/output.tf
@@ -1,11 +1,11 @@
-output "s3-bucket" {
-  value = aws_s3_bucket.s3_bucket.id
-}
+#output "s3-bucket" {
+#  value = aws_s3_bucket.s3_bucket.id
+#}
 
-output "s3-arn" {
-  value = aws_s3_bucket.s3_bucket.arn
-}
+#output "s3-arn" {
+#  value = aws_s3_bucket.s3_bucket.arn
+#}
 
-output "DB" {
-  value = aws_dynamodb_table.terraform_state_lock.id
-}
+#output "DB" {
+#  value = aws_dynamodb_table.terraform_state_lock.id
+#}
